c63253ca71975a61fbf7e0d676495f6a095febb5
Robert Parak <parakr@gmail.com>
2022-05-12 11:28:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ Amplify.configure(config)
 
 ReactDOM.render(<Canvas />, document.getElementById('root'));
 
+let jolo = await fetch("https://5op6o366fb.execute-api.eu-west-1.amazonaws.com/devp/pokus")
+console.log("JOLO",jolo)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
